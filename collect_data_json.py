3b75f8303ec76d6af0c5f0274acd7f9c24bb84d6
Ondřej Senohrábek <admin@ewe.cz>
2026-01-07 10:48:57 +0100

    feature: gzip compression

--- collect_data_json.py
+++ collect_data_json.py
@@ -34,21 +34,21 @@ set_logging(config)
 ###########################################
 
 
-# REST API settings
-api_host = config["RestApi"]["Host"]
-api_port = config["RestApi"]["Port"]
-
-
 ###################################################
 ############# COLLECT CONTROLLER DATA #############
 ###################################################
 
-from typing import Dict
+from typing import Dict, Optional, Any
 from utils import send_request, get_charging_point
 
-def collect_controller_data():
+
+# REST API settings
+api_host = config["RestApi"]["Host"]
+api_port = config["RestApi"]["Port"]
+
+def collect_charger_data() -> Optional[Dict[str, Any]]:
     # Create a dictionary object for output data
-    output_data = {}
+    charger_data = {}
 
     # Get the chargers' controllers from the internal API
     controllers_response = send_request(
@@ -73,7 +73,7 @@ def collect_controller_data():
         charging_point_id, charging_point_name = get_charging_point(controller, charging_point_url)
 
         # Add the controller data to output dictionary
-        output_data[controller] = {
+        charger_data[controller] = {
             "device_name": controllers[controller]["device_name"],
             "controller_uid": controllers[controller]["device_uid"],
             "firmware_version": controllers[controller]["firmware_version"],
@@ -106,9 +106,6 @@ def collect_controller_data():
         ### CHARGING STATE ###
         ######################
 
-        # Vehicle connected states
-        connected_vehicle_state = ["B1", "B2", "C1", "C2", "D1", "D2"]
-
         # Get the connected state the internal API
         connected_state_response = send_request(
             url=f"http://{api_host}:{api_port}/api/v1.0/charging-controllers/{controller}/data?param_list=iec_61851_state",
@@ -121,6 +118,9 @@ def collect_controller_data():
         
         # Get the data from the JSON of the response
         connected_state_data = connected_state_response.json()
+
+        # Vehicle connected states
+        connected_vehicle_state = ["B1", "B2", "C1", "C2", "D1", "D2"]
 
         # Check if vehicle is connected to the charger
         if connected_state_data["iec_61851_state"] in connected_vehicle_state:
@@ -167,18 +167,19 @@ def collect_controller_data():
 
 
         # Add the charging data output dictionary
-        output_data[controller]["charging_data"] = energy["energy"]
-        output_data[controller]["charging_data"]["iec_61851_state"] = connected_state_data["iec_61851_state"]
-        output_data[controller]["charging_data"]["connected_state"] = connected_state
-        output_data[controller]["charging_data"]["connected_time_sec"] = connected_time["connected_time_sec"]
-        output_data[controller]["charging_data"]["charge_time_sec"] = charge_time["charge_time_sec"]
+        charger_data[controller]["charging_data"] = energy["energy"]
+        charger_data[controller]["charging_data"]["iec_61851_state"] = connected_state_data["iec_61851_state"]
+        charger_data[controller]["charging_data"]["connected_state"] = connected_state
+        charger_data[controller]["charging_data"]["connected_time_sec"] = connected_time["connected_time_sec"]
+        charger_data[controller]["charging_data"]["charge_time_sec"] = charge_time["charge_time_sec"]
 
-    return output_data
+    return charger_data
 
 
 #######################################################
 ############# END COLLECT CONTROLLER DATA #############
 #######################################################
+
 
 
 ############################################################
@@ -188,42 +189,37 @@ def collect_controller_data():
 import os
 import json
 import threading
+import gzip
+
 
 # EMM API settings
 emm_api_host = config["EmmSettings"]["Host"]
 emm_api_key = config["EmmSettings"]["ApiKey"]
 emm_headers = {
     "Content-Type": "application/json",
+    "Content-Encoding": "gzip",
     "Authorization": f"Bearer {emm_api_key}",
 }
 
-def controller_data_to_json():
+def charger_data_to_json():
     # Run this function every 5 seconds, start the timer
-    threading.Timer(5, controller_data_to_json).start()
+    threading.Timer(5, charger_data_to_json).start()
 
-    output_data = collect_controller_data()
-
-    # Set the data folder and file name
-    data_folder_path = config["AppSettings"]["FileFolder"]
-
-    # Check if folder for data DOESN'T exists
-    if not os.path.isdir(data_folder_path):
-        # Create the data folder
-        os.makedirs(data_folder_path)
-
-    json_file_name = data_folder_path + "controller_data.json"
-
-    # Save the collected data to JSON file
-    with open(json_file_name, "w", encoding="utf-8") as json_file:
-        json.dump(output_data, json_file)
+    charger_data = collect_charger_data()
 
     # If EMM API is configured also save to EMM web app
     if emm_api_host and emm_api_key:
+        # Convert to JSON string
+        json_data = json.dumps(charger_data)
+
+        # Compress the JSON payload via gzip
+        compressed_data = gzip.compress(json_data.encode('utf-8'))
+
         send_request(
             url=f"{emm_api_host}/api/public/controller-data",
             method="POST",
             headers=emm_headers,
-            data=json.dumps(output_data)
+            data=compressed_data
         )
 
 
@@ -232,4 +228,4 @@ def controller_data_to_json():
 ################################################################
 
 
-controller_data_to_json()
+charger_data_to_json()
